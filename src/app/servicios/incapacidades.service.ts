838330a1fec046a1d09ca77786e87eda75dda9e4
leoespi <riveraleo113@gmail.com>
2024-05-09 10:26:44 -0500

    Imagen Incapacidades(descarga)

--- src/app/servicios/incapacidades.service.ts
+++ src/app/servicios/incapacidades.service.ts
@@ -61,7 +61,7 @@ export class IncapacidadesService {
       'Authorization': 'Bearer ' + access_token
     });
     const options = { headers: headers, responseType: 'blob' as 'json' };
-    return this.http.get<Blob>(`${this.url}/${uuid}/download`, options);
+    return this.http.get<Blob>(`${this.url}/${uuid}/downloadFromDB`, options);
   }
 
    
