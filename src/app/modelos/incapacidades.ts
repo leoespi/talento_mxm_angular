742fccb62c71fcbe9a494d704928a3d65521c1ea
leoespi <riveraleo113@gmail.com>
2024-05-28 13:17:08 -0500

    Incapacidad reportada

--- src/app/modelos/incapacidades.ts
+++ src/app/modelos/incapacidades.ts
@@ -4,6 +4,8 @@ export class Incapacidades {
     cedula:number;
     name :string | null | undefined;
     user: any;
+    
+    tipo_incapacidad_reportada ?: string | null | undefined; 
     dias_incapacidad : number | null | undefined;
     fecha_inicio_incapacidad: Date;
     aplica_cobro : boolean | null | undefined
@@ -14,11 +16,12 @@ export class Incapacidades {
     
 
 
-        constructor(id: number, user_id:number,cedula: number, name:string, dias_incapacidad: number, fecha_inicio_incapacidad: Date, aplica_cobro: boolean, entidad_afiliada:string , tipo_incapacidad:string, user_cedula:number, image:string, uuid: string) {
+        constructor(id: number, user_id:number,cedula: number, name:string, tipo_incapacidad_reportada:string, dias_incapacidad: number, fecha_inicio_incapacidad: Date, aplica_cobro: boolean, entidad_afiliada:string , tipo_incapacidad:string, user_cedula:number, image:string, uuid: string) {
             this.id = id;
             this.user_id = user_id;
             this.cedula = cedula;
             this.name = name;
+            this.tipo_incapacidad_reportada = tipo_incapacidad_reportada;
             this.dias_incapacidad = dias_incapacidad;
             this.fecha_inicio_incapacidad= fecha_inicio_incapacidad;
             this.aplica_cobro = aplica_cobro;
