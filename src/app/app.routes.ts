56af5af7634cdfc79f7111a1e75d005e38b88776
leoespi <riveraleo113@gmail.com>
2024-06-24 14:55:59 -0500

    Aplicacion de guards para superadmin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,26 @@
 import { Routes } from '@angular/router';
-
 import { BodyComponent } from './inicio/body/body.component'; 
-
-import { IndexComponent as IndexUsers} from './users/index/index.component';
-import { IndexComponent as IndexIncapacidades} from './incapacidades/index/index.component';
+import { IndexComponent as IndexUsers } from './users/index/index.component';
+import { IndexComponent as IndexIncapacidades } from './incapacidades/index/index.component';
 import { CreateComponent as CreateIncapacidades } from './incapacidades/create/create.component';
-
-import { IndexComponent as IndexCesantias} from './cesantias/index/index.component';
+import { IndexComponent as IndexCesantias } from './cesantias/index/index.component';
 import { CreateComponent as CreateCesantias } from './cesantias/create/create.component';
-
-import { IndexComponent as IndexCesantiasAutorizadas} from './cesantiasautorizadas/index/index.component';
+import { IndexComponent as IndexCesantiasAutorizadas } from './cesantiasautorizadas/index/index.component';
 import { CreateComponent as CreateCesantiasAutorizadas } from './cesantiasautorizadas/create/create.component';
-
-import { IndexComponent as IndexCesantiasDenegadas} from './cesantiasdenegadas/index/index.component';
-
+import { IndexComponent as IndexCesantiasDenegadas } from './cesantiasdenegadas/index/index.component';
+import { RolesGuard } from './roles.guard';
 
 export const routes: Routes = [
-
-    {path: '', redirectTo:'inicio/body',pathMatch:'full'},
-
-    {path: 'inicio/body', component: BodyComponent },
-
-    {path: 'users/index', component:IndexUsers},
-    {path: 'incapacidades/index', component:IndexIncapacidades},
-    {path: 'incapacidades/create', component:CreateIncapacidades},
-    {path: 'incapacidades/editar/:id', component: CreateIncapacidades },
-     
-    {path: 'cesantias/index', component:IndexCesantias},
-    {path: 'cesantias/create', component:CreateCesantias},
-    {path: 'cesantias/editar/:id', component: CreateCesantias},
-
-    {path: 'cesantiasautorizadas/index', component:IndexCesantiasAutorizadas},
-    {path: 'cesantiasautorizadas/create', component:CreateCesantiasAutorizadas},
-
-    {path: 'cesantiasdenegadas/index', component:IndexCesantiasDenegadas},
-
-    
-
-    
-
-    
-
-    
+  { path: '', redirectTo: 'inicio/body', pathMatch: 'full' },
+  { path: 'inicio/body', component: BodyComponent },
+  { path: 'users/index', component: IndexUsers },
+  { path: 'incapacidades/index', component: IndexIncapacidades },
+  { path: 'incapacidades/create', component: CreateIncapacidades },
+  { path: 'incapacidades/editar/:id', component: CreateIncapacidades },
+  { path: 'cesantias/index', component: IndexCesantias },
+  { path: 'cesantias/create', component: CreateCesantias },
+  { path: 'cesantias/editar/:id', component: CreateCesantias },
+  { path: 'cesantiasautorizadas/index', component: IndexCesantiasAutorizadas, canActivate: [RolesGuard] },
+  { path: 'cesantiasautorizadas/create', component: CreateCesantiasAutorizadas },
+  { path: 'cesantiasdenegadas/index', component: IndexCesantiasDenegadas },
 ];
-
