2491578ec3359a19ad844eb329ba5a1c5f5b2327
leoespi <riveraleo113@gmail.com>
2024-10-02 11:14:55 -0500

    mejores botones sidebar

--- src/app/inicio/menu/menu.component.ts
+++ src/app/inicio/menu/menu.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from '@angular/router'; // Asegúrate de importar RouterModule
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, RouterModule], // Agrega RouterModule aquí
+  imports: [CommonModule, RouterModule],
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'] // Corrige 'styleUrl' a 'styleUrls'
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
   clave: string | null = null;
@@ -18,6 +18,11 @@ export class MenuComponent {
   ngOnInit(): void {
     this.clave = localStorage.getItem('clave');
     this.rolId = parseInt(localStorage.getItem('rolId') || '0', 10); // Parsea el rolId a número
+  }
+
+  // Método para navegar a diferentes rutas
+  navigate(path: string): void {
+    this.router.navigate([path]);
   }
 
   logout(): void {
