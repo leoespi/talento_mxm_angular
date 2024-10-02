0f10ebda7486d58ccc7ed31d7d0f31562f7fd038
leoespi <riveraleo113@gmail.com>
2024-10-02 14:57:30 -0500

    home_card  (flayers)

--- src/app/home/index/index.component.ts
+++ src/app/home/index/index.component.ts
@@ -1,37 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
 
 @Component({
   selector: 'app-index',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './index.component.html',
-  styleUrl: './index.component.scss'
+  styleUrls: ['./index.component.scss']
 })
-export class IndexComponent {
+export class IndexComponent implements OnInit {
 
-  
   id: string | null;
-
   token: string | null = null;
   clave: string | null = null;
   
-  
-  constructor( private router: Router, private aRouter: ActivatedRoute) {
+  // Reemplaza estas URLs con las URLs de tus imágenes
+  images: string[] = [
+    'assets/img/flayer1.jpg',
+    'assets/img/flayer2.jpg',
+    'assets/img/flayer3.jpg',
+
+    
+  ];
+  currentIndex: number = 0;
+
+  constructor(private router: Router, private aRouter: ActivatedRoute) {
     this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
-  
   ngOnInit(): void {
     this.recuperarToken();
-    
+    this.startImageRotation();
   }
 
-  
-  // Recupera el token del almacenamiento local
   recuperarToken(): void {
     this.token = localStorage.getItem('clave');
     if (this.token == null) {
@@ -39,5 +42,9 @@ export class IndexComponent {
     }
   }
 
-
+  startImageRotation(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 8000); // Cambia la imagen cada 10 segundos
+  }
 }
